dc09689677f35cf74a52a75a3aa69b9ada1f1665
Rajesh Sehgal <rajeshsehgal@impauto.com>
2023-12-18 12:30:00 +0000

    asn

--- app/schedulingagreement/webapp/controller/SAAsnCreate.controller.js
+++ app/schedulingagreement/webapp/controller/SAAsnCreate.controller.js
@@ -803,7 +803,7 @@ sap.ui.define([
 					}
 
 				}
-				oModel.create("/ASNListHeader", ASNHeaderData, null, function (oData, response) {
+				oModel.update("/ASNListHeader", ASNHeaderData, null, function (oData, response) {
 					MessageBox.success("ASN created succesfully");
 					
 
@@ -817,7 +817,7 @@ sap.ui.define([
 					}
 				});
 				for (var i = 0; i < ASNItemData.length; i++) {
-					oModel.create("/ASNList", ASNItemData[i], null, function (oData, response) {
+					oModel.update("/ASNList", ASNItemData[i], null, function (oData, response) {
 						
 						MessageBox.success("ASN created succesfully  ", {
 							actions: [sap.m.MessageBox.Action.OK],
